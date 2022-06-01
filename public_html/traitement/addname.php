a3022ebd675890d7017107007006f97492544886
Aurore-Grimault <auroregrimault@hotmail.fr>
2022-06-01 09:45:59 +0200

    commit 1

--- public_html/traitement/addname.php
+++ public_html/traitement/addname.php
@@ -5,7 +5,10 @@ if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     require("../config/db.php");
     $addname = filter_var($_POST['addname'], FILTER_SANITIZE_SPECIAL_CHARS);
     print_r($user['id']);
-    $stmt = $dbh->prepare('UPDATE user SET name=($_POST[addname]) WHERE id=$_POST[id]');
+    $stmt = $dbh->prepare('UPDATE login SET name=:name WHERE id=:id');
+    $stmt -> bindValue(':name',$_POST['name'],PDO::PARAM_STR);
+    $stmt -> bindValue(':id',$_POST['id'],PDO::PARAM_INT);
+   
     // "UPDATE MyGuests SET lastname='Doe' WHERE id=2";
     $stmt->execute();
     echo 'mise à jour du prénom effectuée';
